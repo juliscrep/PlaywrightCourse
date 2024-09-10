5eeb53fa3fe806de584d10deda2b694751511311
juliscrep <julietascrepnik@gmail.com>
2024-09-10 12:02:22 -0300

    Added another example of mocking

--- tests/APITests/mockExample.spec.ts
+++ tests/APITests/mockExample.spec.ts
@@ -11,3 +11,22 @@ test("Genera un mock de una fruta que no viene de la API real ", async ({ page }
     // Validamos que Melocotón está disponible
     await expect(page.getByText('Melocotón')).toBeVisible();
 });
+
+
+test('Obtengo la respuesta real y le agrego algo no tan real', async ({ page }) => {
+    
+    await page.route('*/**/api/v1/fruits', async route => {
+        const response = await route.fetch();
+        const json = await response.json();
+        json.push({ name: 'Coconut', id: 200 });
+        // Obtenemos la respuesta real mientras que le agregamos un extra
+        // al objeto JSON que va a estar siendo representado.
+        await route.fulfill({ response, json });
+    });
+ 
+    
+    await page.goto('https://demo.playwright.dev/api-mocking');
+ 
+    // Validamos que vino la respuesta real con el extra que le sumamos antes
+    await expect(page.getByText('Coconut', { exact: true })).toBeVisible();
+});
