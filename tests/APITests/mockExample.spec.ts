f0baa74d2a0150f3e46f56cee2cb1995454011b7
juliscrep <julietascrepnik@gmail.com>
2024-09-10 11:41:42 -0300

    Added an Example of Mocking

---
+++ tests/APITests/mockExample.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+test("Genera un mock de una fruta que no viene de la API real ", async ({ page }) => {
+    // Hacemos un mock de la API antes de navegar
+    await page.route('*/**/api/v1/fruits', async route => {
+        const json = [{ name: 'Melocotón', id: 26 }];
+        await route.fulfill({ json });
+    });
+    
+    await page.goto('https://demo.playwright.dev/api-mocking');
+ 
+    // Validamos que Melocotón está disponible
+    await expect(page.getByText('Melocotón')).toBeVisible();
+});
